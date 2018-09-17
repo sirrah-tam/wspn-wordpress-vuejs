16a48235a30e87e1765287b6c65bd1a6ee099cf3
Mat Harris <mat.harris@efelle.com>
2018-09-16 17:54:39 -0700

    Update header component, add styles

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -1,34 +1,22 @@
 <template>
-    <b-navbar toggleable="md" type="dark" variant="dark">
+  <b-navbar toggleable="md" fixed="top" class="page-header">
+    <b-container>
 
-        <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
 
-        <b-navbar-brand to="/">Vuejs Wordpress Theme Starter</b-navbar-brand>
+      <b-navbar-brand to="/"><img src="/wp-content/themes/vuejs-theme/dist/images/WASPN.png" alt="waspn logo" /></b-navbar-brand>
 
-        <b-collapse is-nav id="nav_collapse">
+      <b-collapse is-nav id="nav_collapse">
 
-            <b-nav is-nav-bar>
-            <b-nav-item href="#">Link</b-nav-item>
-            </b-nav>
+        <!-- Right aligned nav items -->
+        <b-nav is-nav-bar class="ml-auto">
+          <b-nav-item href="#">Details</b-nav-item>
+          <b-nav-item href="#">Accessibility</b-nav-item>
+          <b-nav-item href="#">Sponsors</b-nav-item>
+          <b-nav-item href="#">Contact</b-nav-item>
+        </b-nav>
 
-            <!-- Right aligned nav items -->
-            <b-nav is-nav-bar class="ml-auto">
-
-            <b-nav-form>
-                <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
-
-            <b-nav-item-dropdown right>
-                <!-- Using button-content slot -->
-                <template slot="button-content">
-                <em>User</em>
-                </template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item href="#">Signout</b-dropdown-item>
-            </b-nav-item-dropdown>
-            </b-nav>
-
-        </b-collapse>
-    </b-navbar>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
 </template>
