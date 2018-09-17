84bab3447722e9e3c44d1c9fd68fda511f19ac94
Mat Harris <mat.harris@efelle.com>
2018-09-16 17:56:38 -0700

    Add components for homepage sections

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,41 +1,43 @@
 <template>
-    <b-container class="bv-example-row">
-        <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2" >
-        <p>For more information visit website</p>
-        <b-btn variant="primary" href="https://bootstrap-vue.js.org" target="_blank">More Info</b-btn>
-        </b-jumbotron>
+  <b-container class="bv-example-row">
+    <template v-if="allPagesLoaded">
+      <b-jumbotron :header="pageContent.title.rendered" class="text-center">
+        <div class="page-content" v-html="pageContent.content.rendered">
 
-        <b-row>
-            <b-col>
-                <recent-posts-widget limit="5">Recent Posts</recent-posts-widget>
-            </b-col>
-            <b-col>
-                <pages-widget limit="5">Pages</pages-widget>
-            </b-col>
-            <b-col>
-                <h3>Welcome!</h3>
-                <p>You'll probably want to remove all of this stuff, but hey it at least gives you a preview of Bootstrap-Vue!</p>
-                <p>I would encourage you to avoid using jQuery and instead find the Vue way of doing some things. You'll thank me later!</p>
-                <p>~ <a href="http://evanagee.com/">Evan Agee</a></p>
-            </b-col>
-        </b-row>
-    </b-container>
+        </div>
+      </b-jumbotron>
+    </template>
+  </b-container>
 </template>
 
 <script>
-import RecentPostsWidget from './widgets/RecentPosts'
-import PagesWidget from './widgets/Pages'
+import { mapGetters } from 'vuex'
+import Details from './partials/Details'
+import Sponsors from './partials/Sponsors'
+import Contact from './partials/Contact'
 
 export default {
-    components: {
-        RecentPostsWidget,
-        PagesWidget
+  components: {
+    Details,
+    Sponsors,
+    Contact
+  },
+
+  computed: {
+    ...mapGetters({
+      page: 'page',
+      allPagesLoaded: 'allPagesLoaded'
+    }),
+
+    pageContent() {
+      return this.page('home-main')
     }
+  },
 }
 </script>
 
 <style lang="scss">
-    .jumbotron {
-        margin-top: 5%;
-    }
+.jumbotron {
+  margin-top: 5%;
+}
 </style>
