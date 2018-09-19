5f15f52113ac6a82ed5daf04b3550eaf0d36675e
Mat Harris <hurris88@gmail.com>
2018-09-18 23:11:27 -0700

    Add accessibility section

---
+++ src/components/sections/Accessibility.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <b-jumbotron id="accessibility" class="text-white" bg-variant="secondary">
+    <div class="section-content">
+      <h2>Accessibility</h2>
+      <hr>
+      <div class="accessibility__content" v-html="pageContent.content.rendered">
+
+      </div>
+      <hr>
+    </div>
+  </b-jumbotron>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+
+    computed: {
+      ...mapGetters({
+        page: 'page'
+      }),
+
+      pageContent() {
+        return this.page('accessibility')
+      }
+    }
+}
+</script>
