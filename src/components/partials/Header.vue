8cb27ae852587ed6d3535e58e2826912ea933c52
Mat Harris <hurris88@gmail.com>
2018-09-18 22:58:31 -0700

    Header styles/template

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -2,21 +2,21 @@
   <b-navbar toggleable="md" fixed="top" class="page-header">
     <b-container>
 
-      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
-
       <b-navbar-brand to="/"><img src="/wp-content/themes/wspn-wordpress-vuejs/dist/assets/WASPN.png" alt="waspn logo" /></b-navbar-brand>
 
       <b-collapse is-nav id="nav_collapse">
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item href="#details" v-smooth-scroll="{target: '#details'}">Details</b-nav-item>
-          <b-nav-item href="#">Accessibility</b-nav-item>
-          <b-nav-item href="#sponsors" v-smooth-scroll>Sponsors</b-nav-item>
-          <b-nav-item href="#contact" v-smooth-scroll>Contact</b-nav-item>
-        </b-navbar-nav>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"><a href="#details" class="nav-link" v-smooth-scroll="{ offset: -140 }">Details</a></li>
+          <li class="nav-item"><a href="#accessibility" class="nav-link">Accessibility</a></li>
+          <li class="nav-item"><a href="#sponsors" class="nav-link" v-smooth-scroll="{ offset: -140 }">Sponsors</a></li>
+          <li class="nav-item"><a href="#contact" class="nav-link" v-smooth-scroll="{ offset: -140 }">Contact</a></li>
+        </ul>
 
       </b-collapse>
+
+      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
     </b-container>
   </b-navbar>
 </template>
