0f3ec111514ace46919173079e08e44a88ee4b0d
Mat <matharris@sirrahtam.com>
2018-10-16 19:38:53 -0700

    General style updates

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,7 +3,6 @@ try {
   window.$ = window.jQuery = require('jquery');
   window.axios = require('axios');
   window.axios.defaults.headers.common = {
-    'X-CSRF-TOKEN': window.WordPress.csrfToken,
     'X-Requested-With': 'XMLHttpRequest'
   };
 } catch (e) {
